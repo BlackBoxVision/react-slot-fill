ffe51af0b621d8295bc1e2cd78cabcdb52e2967c
jonatansalas <jonatansalas@live.com.ar>
2018-09-01 11:22:19 -0300

    Better simple example code

--- examples/basic-usage/src/App.js
+++ examples/basic-usage/src/App.js
@@ -5,14 +5,22 @@ import SF from 'react-slot-and-fill';
 export default class App extends React.Component {
     render() {
         return (
-            <SF.Provider>
-                <SF.Slot id="mySlot" />
-                <SF.Fill id="mySlot">
-                    <div style={{ width: 300, height: 300, backgroundColor: "red", display: "flex", alignItems: "center", justifyContent: "center", color: "white" }}>
-                        Hello World
-                    </div>
-                </SF.Fill>
-            </SF.Provider>
+            <div className="container">
+                <SF.Provider>
+                    <SF.Slot id="red-box" />
+                    <SF.Fill id="red-box">
+                        <div className="container with-dimensions red">
+                            Red Box
+                        </div>
+                    </SF.Fill>
+                    <SF.Fill id="green-box">
+                        <div className="container with-dimensions green">
+                            Green Box
+                        </div>
+                    </SF.Fill>
+                    <SF.Slot id="green-box" />
+                </SF.Provider>
+            </div>
         );
     }
 }
